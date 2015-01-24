f05710a4b3a3cfd10a014e37ab8adde157dd13c0
William Dyce <wilbefast@gmail.com>
2015-01-24 12:37:01 +0100

    examples characters and clothes

--- src/assets/lson/characters.lua
+++ src/assets/lson/characters.lua
@@ -1,4 +1,17 @@
 return {
-	
-	
+	bear = {
+
+		"burka",
+		"bear_fur",
+
+		orgy = -100 -- orgies are unlikely if there are bears
+	},
+
+	osama = {
+		"beard",
+		"tunic",
+		"ak47",
+
+		jihad = 10 -- osama causes jihad
+	}
 }
