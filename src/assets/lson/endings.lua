748de25de5fb4c5fc0bc0c0b8dd0a36a1e910a90
MarechalBanane <guillaume.faure@lecortex.com>
2015-01-26 14:42:24 +0100

    update endings and props

--- src/assets/lson/endings.lua
+++ src/assets/lson/endings.lua
@@ -20,31 +20,43 @@ return {
 	{
 		name = "orgy",
 		trigger = 50,
-		text = "an ORGY !",
+		text = "ORGY !",
 		sound = "FinOrgie"
 	},
 	{
 		name = "gamer",
 		trigger = 50,
-		text = "a GAMER PARTY !",
+		text = "GAMER PARTY !",
 		sound = "FinLAN"
 	},
 	{
 		name = "hippie",
 		trigger = 50,
-		text = "a HIPPIE GATHERING !",
+		text = "HIPPIE GATHERING !",
 		sound = "FinHippie"
 	},
 	{
 		name = "jihad",
 		trigger = 50,
-		text = "a JIHAD !",
+		text = "JIHAD !",
 		sound = "FinDjihad"
+	},
+	{
+		name = "puke",
+		trigger = 50,
+		text = "OPEN BAR !",
+		sound = "FinPuke"
+	},
+	{
+		name = "pizza",
+		trigger = 50,
+		text = "PIZZA TIME !",
+		sound = "FinPizza"
 	},
 	{ -- the last ending is the ending by default
 		name = "normal",
 		trigger = 50,
-		text = "a PIZZA PARTY !",
+		text = "PARTY !",
 		sound = "FinNormale"
 	}
 	
